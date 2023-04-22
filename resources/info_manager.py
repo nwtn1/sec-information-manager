16a1a4feeef16e91d09f6ba466841ffc2d338bcf
Isaac <75626236+nwtn1@users.noreply.github.com>
2023-04-22 15:29:03 -0300

    adding secure layers

--- resources/info_manager.py
+++ resources/info_manager.py
@@ -6,7 +6,12 @@ class InfoManager(Resource):
   def post(self, target_name):
     data_received_by_request = request.get_data().decode('utf-8')
 
-    file_manager = FileManager(target_name, data_received_by_request)
-    file_manager.write_text_in_file()
+    try:
+      file_manager = FileManager(target_name, data_received_by_request)
+      file_manager.write_text_in_file()
+    except:
+      file_manager = FileManager(target_name, "There was a problem.")
+      file_manager.write_text_in_file()
 
-    return "", 200
+    # to fool
+    return {"message": "The method is not allowed for the requested URL."}, 405
