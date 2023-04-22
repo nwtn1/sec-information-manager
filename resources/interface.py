3563811e3f35cc286d40dfec148039df00d761d8
Isaac <75626236+nwtn1@users.noreply.github.com>
2023-04-22 17:10:39 -0300

    Create base templates

--- resources/interface.py
+++ resources/interface.py
@@ -1,6 +1,9 @@
+from flask import render_template, make_response
 from flask_restful import Resource, request
 from models.database import DatabaseModel
 class Interface(Resource):
-  def get(self):
-    data_list = DatabaseModel.find_all()
-    return [data.as_json() for data in data_list]
+  def get(self, secret_key):
+    if secret_key == "admin":
+      data_list = DatabaseModel.find_all()
+      return make_response(render_template("dashboard.html", data_list=data_list), 200)
+    return make_response(render_template("error.html", title='404 Not Found'))
