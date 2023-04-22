c1cb91195fd25954597779cac232e914516176de
Isaac <75626236+nwtn1@users.noreply.github.com>
2023-04-21 22:04:16 -0300

    set structure

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from flask_restful import Api, request, Resource
+from flask_restful import Api
 
+from resources.tools import InfoManager
+from resources.interface import Interface
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.before_request
-def before_request():
-  return ""
+api.add_resource(InfoManager, "/receive/<target_name>")
+api.add_resource(InfoManager, "/show")
 
 
 if __name__ == '__main__':
---
+++ resources/tools.py
@@ -0,0 +1,5 @@
+from flask_restful import Resource, request
+
+class InfoManager(Resource):
+  def post(self):
+    return ""
