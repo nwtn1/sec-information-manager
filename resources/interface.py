4cc4e4babcabf1abe600716091b0e505ece10217
Isaac <75626236+nwtn1@users.noreply.github.com>
2023-04-22 17:54:22 -0300

    Basic dashbaord template

--- resources/interface.py
+++ resources/interface.py
@@ -2,8 +2,10 @@ from flask import render_template, make_response
 from flask_restful import Resource, request
 from models.database import DatabaseModel
 class Interface(Resource):
+
   def get(self, secret_key):
     if secret_key == "admin":
       data_list = DatabaseModel.find_all()
       return make_response(render_template("dashboard.html", data_list=data_list), 200)
-    return make_response(render_template("error.html", title='404 Not Found'))
+
+    return make_response(render_template("error.html"))
